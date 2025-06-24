8c12e867495f3bb9450cbfbbd0f1af5958fb11ff
joseongnam <jsn4873@gmail.com>
2025-06-25 08:15:39 +0900

    슬라이드최종

--- src/App.jsx
+++ src/App.jsx
@@ -8,68 +8,84 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [page, setPage] = useState('one');
-  const pageToIndex = { one: 0, two: 1, three: 2 };
-  const translateX = `translateX(-${pageToIndex[page] * 100}vw)`;
+  const pageList = ['one', 'two', 'three'];
+  const pageIndex = pageList.indexOf(page);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPage((prev) => {
-        const nextIndex = (pageList.indexOf(prev) + 1) % pageList.length;
-        return pageList[nextIndex];
-      });
+      const nextIndex = (pageIndex + 1) % pageList.length;
+      setPage(pageList[nextIndex]);
     }, 5000);
-  
+
     return () => clearInterval(interval);
   }, [page]);
 
   return (
     <>
-    <nav className='up-navbar'>
-      <div className='event-nav'>
-        <h7>👑이달의 리뷰왕! 6월 리뷰 이벤트 보러가기</h7>
-      </div>
-      <div className='main-nav'>
-        <div className='box' style={{fontWeight: 'bold', fontSize : '30px'}}>YOGO</div>
-        <div className='box'>
-          <ul>
-            <li>요고특가</li>
-            <li>모든상품</li>
-            <li>리뷰</li>
-            <li>이벤트</li>
-            <li>고객센터</li>
-          </ul>
+      <nav className="up-navbar">
+        <div className="event-nav">
+          <h7>👑이달의 리뷰왕! 6월 리뷰 이벤트 보러가기</h7>
         </div>
-        <div className='box' >
-        <span>로그인</span>
-        <span>회원가입</span>
-          <span><FontAwesomeIcon icon={faUser} /></span>
-          <span><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
-          <span><FontAwesomeIcon icon={faBagShopping} /></span>
+        <div className="main-nav">
+          <div className="box" style={{ fontWeight: 'bold', fontSize: '30px' }}>YOGO</div>
+          <div className="box">
+            <ul>
+              <li>요고특가</li>
+              <li>모든상품</li>
+              <li>리뷰</li>
+              <li>이벤트</li>
+              <li>고객센터</li>
+            </ul>
           </div>
+          <div className="box">
+            <span>로그인</span>
+            <span>회원가입</span>
+            <span><FontAwesomeIcon icon={faUser} /></span>
+            <span><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
+            <span><FontAwesomeIcon icon={faBagShopping} /></span>
+          </div>
+        </div>
+      </nav>
+
+      <div className="main-container">
+        {/* 양옆 화살표 버튼 */}
+        <div
+          className="start-container"
+          style={{
+            transform: `translateX(-${pageIndex * 100}vw)`,
+            transition: 'transform 0.5s',
+          }}
+        >
+          {pageList.map((_, index) => (
+            <div className="slide-box" key={index}>
+              <img src={`/img/car${index + 1}-1.png`} alt={`슬라이드${index + 1}`} />
+            </div>
+          ))}
+        </div>
+
+        <div className="btn-container">
+          {pageList.map((num, index) => (
+            <button
+              onClick={() => setPage(num)}
+              key={index}
+              className={page === num ? 'active' : ''}
+            ></button>
+          ))}
+        </div>
+
+        {/* 화살표 버튼 */}
+        <div className="arrow left" onClick={() => {
+          const prevIndex = (pageIndex - 1 + pageList.length) % pageList.length;
+          setPage(pageList[prevIndex]);
+        }}>&lt;</div>
+
+        <div className="arrow right" onClick={() => {
+          const nextIndex = (pageIndex + 1) % pageList.length;
+          setPage(pageList[nextIndex]);
+        }}>&gt;</div>
       </div>
-    </nav>
-    <div className='main-container'>
-  <div className='start-container' style={{ transform: translateX, transition: 'transform 0.5s' }}>
-    {['one','two','three'].map((num, index) => (
-      <div className='slide-box' key={index}>
-        <img src={`/img/car${index+1}-1.png`} alt={`슬라이드${num}`} />
-      </div>
-    ))}
-  </div>
-  <div className="btn-container">
-    {['one','two','three'].map((num, index) => (
-      <button
-        onClick={() => setPage(num)}
-        key={index}
-        className={page === num ? 'active' : ''}
-      ></button>
-    ))}
-  </div>
-</div>
-      <div className='middle-container'></div>
-      <div className='end-container'></div>
     </>
-  )
+  );
 }
 
 export default App;
